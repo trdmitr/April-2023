5af5b4dea5e28681f302777a3972b01b0e2cab91
trdmitr <trdmitr@gmail.com>
2023-05-08 00:18:54 +0400

    Updates

---
+++ precache-manifest.165cb996842c7ed8e557ef4b4d8c06e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a73873ccd7f906d4c82",
+    "url": "/April-2023/static/css/main.d35e3c20.chunk.css"
+  },
+  {
+    "revision": "9a73873ccd7f906d4c82",
+    "url": "/April-2023/static/js/main.47082fba.chunk.js"
+  },
+  {
+    "revision": "c6cdab9dd1e800360a63",
+    "url": "/April-2023/static/js/runtime~main.c601468e.js"
+  },
+  {
+    "revision": "d93fb7ea7a36266a7999",
+    "url": "/April-2023/static/css/2.1959ed21.chunk.css"
+  },
+  {
+    "revision": "d93fb7ea7a36266a7999",
+    "url": "/April-2023/static/js/2.f71002a7.chunk.js"
+  },
+  {
+    "revision": "d4a9377eb37124be268f",
+    "url": "/April-2023/static/js/3.fcc995d3.chunk.js"
+  },
+  {
+    "revision": "f57c1202a8a62acee8a56622412c3e5b",
+    "url": "/April-2023/static/media/jakobctt_8b806cbf869f1c45daf1949a025cd519.f57c1202.woff"
+  },
+  {
+    "revision": "446e455180c7b116a1fbc3de7408d425",
+    "url": "/April-2023/index.html"
+  }
+];
